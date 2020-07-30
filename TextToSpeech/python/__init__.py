eeadc681e735e3893e4d57925e6223364585d41a
Ankit Dwivedi <adwivedi@microsoft.com>
2020-07-29 18:02:46 -0700

    make api work on Azure web app (#4)

---
+++ TextToSpeech/python/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask, request
+
+webapp = Flask(__name__) #create the Flask app
---
+++ TextToSpeech/python/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update
+apt-get -y install poppler-utils
+apt-get -y install tesseract-ocr
+apt-get -y install tesseract-ocr-hin
